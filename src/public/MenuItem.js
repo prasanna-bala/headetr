1c8ae2551d9a20db216f17a05d232b6b37cb37fe
Prasanna BALASUBRAMANIYAM <94684396+prasanna-bala@users.noreply.github.com>
2022-02-12 19:58:32 +0100

    Add files via upload

---
+++ src/public/MenuItem.js
@@ -0,0 +1,25 @@
+export const MenuItems =
+    [
+        {
+            title: 'Home',
+            path: '#',
+            cName: 'nav-links'
+        },
+
+        {
+            title: 'Services',
+            path: '#',
+            cName: 'nav-links'
+        },
+        {
+            title: 'Contact Us',
+            path: '#',
+            cName: 'nav-links'
+        },
+
+        {
+            title: 'Sign Up',
+            path: '#',
+            cName: 'nav-links-mobile'
+        }
+    ]
